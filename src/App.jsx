018e4f733e73dbee53c9b1fd785f9fc73766a3ad
wristwatch’s <bm8781104@gmail.com>
2025-12-29 22:36:47 +0100

    SECURE: Locked admin page route in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import ScrollToTop from './components/ScrollToTop'; 
+import ProtectedRoute from './components/ProtectedRoute'; // <-- IMPORT THIS
 
 // Pages
 import Home from './pages/Home';
@@ -20,27 +21,31 @@ import Checkout from './pages/Checkout';
 function App() {
   return (
     <div className="flex flex-col min-h-screen font-sans text-gray-900">
-      {/* ScrollToTop works because the Router is in main.jsx */}
       <ScrollToTop />
       
       <Navbar />
       
       <main className="flex-grow">
         <Routes>
-          {/* PUBLIC PAGES */}
+          {/* PUBLIC PAGES - Accessible to everyone */}
           <Route path="/" element={<Home />} />
           <Route path="/services" element={<Services />} />
           <Route path="/blog" element={<Blog />} />
           <Route path="/blog/:id" element={<PostDetails />} />
-          
-          {/* ADMIN & AUTH */}
-          <Route path="/admin" element={<Admin />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
-
-          {/* PAYMENT PAGE */}
           <Route path="/checkout" element={<Checkout />} />
+          
+          {/* SECURE ADMIN PAGE - ONLY accessible with a password */}
+          <Route 
+            path="/admin" 
+            element={
+              <ProtectedRoute>
+                <Admin />
+              </ProtectedRoute>
+            } 
+          />
         </Routes>
       </main>
       
